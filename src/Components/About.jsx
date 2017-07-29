e4aa9318cc66c7916896e29575e9fa7faf6420c5
Tabitha Hsia <tabithahsia@Tabithas-MacBook-Pro.local>
2017-07-29 16:58:20 -0700

    update styling

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -21,15 +21,15 @@ class About extends React.Component {
           <br></br>
           I enjoy interesting challenges. :)
           <br></br>
-          I have also played violin for 20 years.
+          I have also played violin for 20 years and am an active musician.
           <br></br>
           I live with my pet fish, Einstein Bettafin Hsia.
         </p>
-        <h3> Skills </h3>
+        <h3 id="about"> Skills </h3>
         <p><strong>Strong</strong>: Javascript ES6, HTML, React, Node, Express, MySQL, SQL, Redux </p>
         <p><strong>Knowledgeable</strong>: CSS, Backbone, jQuery, PostgreSQL, React-Router, Heroku, MongoDB, Mongoose, Bluebird, Knex, Git, C++, Angular, Socket.io, WebSockets, Jekyll, D3</p>
 
-        <h3> Education </h3>
+        <h3 id="about"> Education </h3>
         <p><strong>University of the Pacific, </strong>  B.A. in Music Violin & Asian Languages and Studies: Chinese, Minor in Pre-Law
           <br></br>
           <span id="italicize">Relevant Courses: Intro to Computer Science (C++), Data Structures</span>
@@ -40,7 +40,7 @@ class About extends React.Component {
         <span id="italicize">Relevant Courses: Calculus I, Calculus II, Linear Algebra</span>
         </p>
 
-        <h3>Fun Facts</h3>
+        <h3 id="about">Fun Facts</h3>
         <li>My dream job is to be the Mickey Mouse in Fantasmic</li>
         <li>Favorite aquatic animal is the whale shark</li>
         <li>I am a curry addict</li>
