070c071b44eab84db622d1c7efb17285066b7ffd
Tabitha Hsia <tabithahsia@Tabithas-MBP.attlocal.net>
2017-08-07 23:53:42 -0700

    Update music

--- src/Components/Music.jsx
+++ src/Components/Music.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import Prokofiev from '../assets/music/Prokofiev.mp3';
 import ReactAudioPlayer from 'react-audio-player';
+
+//Pieces
+import Prokofiev from '../assets/music/Prokofiev.mp3';
+import Wieniawski2 from '../assets/music/Wieniawski Violin Concerto no. 2 mvt 3.mp3';
+import Melodie from '../assets/music/Tchaikovsky Melodie.mp3';
 
 
 class Music extends React.Component {
@@ -16,10 +20,22 @@ class Music extends React.Component {
       <div>
         <h2>Violin Music</h2>
 
-      <h3>Prokofiev Violin Concerto 1, 2nd Movement</h3>
+        <h3>Prokofiev Violin Concerto 1, 2nd Movement</h3>
           <ReactAudioPlayer
             src={Prokofiev}
             controls
+          />
+
+        <h3>Wieniawski Violin Concerto 2, 3rd Movement</h3>
+          <ReactAudioPlayer
+            src={Wieniawski2}
+            controls
+          />
+
+        <h3>Tchaikovsky Melodie (No. 3)</h3>
+          <ReactAudioPlayer
+            src={Melodie}
+            controle
           />
       </div>
     )
