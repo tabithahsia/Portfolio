df97330d6339749bd4501aab1d42db8002f8b1dd
Tabitha <tabithahsia@gmail.com>
2019-05-25 21:44:37 -0400

    Updated info

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Selfie4 from '../assets/images/Selfie4.JPG';
+import Selfie6 from '../assets/images/Selfie6.jpg';
 
 class About extends React.Component {
   constructor(props) {
@@ -14,24 +14,24 @@ class About extends React.Component {
       <div>
         <h2>About Me</h2>
         <br></br>
-        <img src={Selfie4} alt="Selfie4" id="selfie4"/>
+        <img src={Selfie6} alt="Selfie6" id="selfie6"/>
         <br></br>
         <br></br>
         <p>
-          Hello there! My name is Tabitha Hsia. I am a Full-Stack Software Engineer based in Redmond, WA.
+          Hello there! My name is Tabitha Hsia. I am a Full-Stack Software Engineer based in the Greater Seattle Area.
           <br></br>
           I enjoy challenging myself through learning new technologies and implementing them in exciting projects.
           <br></br>
           I have also played violin for over 20 years and am an active musician.
           <br></br>
           <br></br>
-          I am currently a Software Design Engineer (Apprentice) @ <a target="_blank" href="https://www.microsoft.com/en-us/Microsoft">Microsoft</a> and a Junior Developer @ <a target="_blank" href="https://soundiron.com/">Soundiron</a>
+          I am currently a Software Engineer @ <a target="_blank" href="https://www.microsoft.com/en-us/Microsoft">Microsoft</a> and a Junior Developer @ <a target="_blank" href="https://soundiron.com/">Soundiron</a>
           <br></br>
           I also volunteer as a Front End Developer @ <a target="_blank" href="https://www.sheltertech.org/">Sheltertech</a>
         </p>
         <a target="_blank" href ="http://bit.ly/tabithahsiaresume">Tech Resume</a> | <a target="_blank" href="http://bit.ly/tabithahsiamusicresume">Music Resume</a>
         <h3 id="about"> Skills </h3>
-        <p><strong>Strong</strong>: JavaScript ES6, ReactJS, Redux, Webpack, Node.js, Express, SQL, PostgreSQL, SQLite, MySQL, MongoDB, Mongoose, Git, HTML5, CSS3, jQuery, Bluebird</p>
+        <p><strong>Strong</strong>: TypeScript, JavaScript ES6, C#, ReactJS, Redux, Webpack, Node.js, Express, SQL, PostgreSQL, SQLite, MySQL, MongoDB, Mongoose, Git, HTML5, CSS3, jQuery, Bluebird</p>
         <p><strong>Knowledgeable</strong>: AngularJS, Python, Backbone.js, D3.js, C++, WebSockets, Socket.IO, Jekyll, Heroku, AWS, KSP, Ruby</p>
 
         <h3 id="about"> Education </h3>
@@ -45,7 +45,7 @@ class About extends React.Component {
           <span id="italicize">Relevant Courses: Calculus I, Calculus II, Linear Algebra</span>
         </p>
         <h3 id="about">Fun Facts</h3>
-        <li>I am a curry addict</li>
+        <li>I am a curry and fried chicken addict</li>
         <li>Favorite aquatic animal is the whale shark</li>
         <li>My dream job is to be the Mickey Mouse in Fantasmic</li>
 
