b017d94393e1e9519414c84ebcb3fb76cec3d10b
Tabitha <tabithahsia@gmail.com>
2020-05-24 10:29:17 -0700

    Removed unused code

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 
 class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      phone: '',
-      message: ''
-    }
-  }
-
   render() {
     return (
       <div>
@@ -18,22 +8,22 @@ class Contact extends React.Component {
         <p> Contact me at <a href="mailto:tabithahsia@gmail.com">tabithahsia@gmail.com</a> </p>
         <p> or fill out the form below </p>
         <form action="https://formspree.io/tabithahsia@gmail.com" method="POST" class="submissionfield">
-          <input type="text" name="Name" placeholder="Your Name* " value={this.state.name} onChange={(e) => this.setState({ name: e.target.value})} required/>
+          <input type="text" name="Name" placeholder="Your Name* " required/>
 
           <br></br>
           <br></br>
 
-          <input type="text" name="Email" placeholder="Your Email* " value={this.state.email} onChange={(e) => this.setState({ email: e.target.value})} required />
+          <input type="text" name="Email" placeholder="Your Email* " required />
 
           <br></br>
           <br></br>
 
-          <input type="text" name="Phone Number" placeholder="Your Phone (optional)" value={this.state.phone} onChange={(e) => this.setState({ phone: e.target.value})} />
+          <input type="text" name="Phone Number" placeholder="Your Phone (optional)" />
 
           <br></br>
           <br></br>
 
-          <textarea name="Message" placeholder="Your Message*" value={this.state.message} onChange={(e) => this.setState({ message: e.target.value})} required/>
+          <textarea name="Message" placeholder="Your Message*" required/>
 
           <br></br>
           <br></br>
