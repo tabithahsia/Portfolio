ac58b741beb650774a14141a27bd65e8180b566c
Tabitha <tabithahsia@gmail.com>
2024-10-21 18:52:45 -0700

    Cleaned up About page code

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,31 +1,21 @@
 import React from 'react';
 import CatWithCoffee from '../assets/images/CatWithCoffee.jpg';
 
-class About extends React.Component {
-  render() {
+const About = () => {
     return (
       <div>
         <h2>About Me</h2>
-        <br></br>
         <img src={CatWithCoffee} alt="CatWithCoffee" id="CatWithCoffee"/>
-        <br></br>
-        <br></br>
-        <p>
-          Hello there! My name is Tabitha Hsia. I am a Full-Stack Software Engineer currently on a career break.
-          <br></br>
-          I enjoy challenging myself through learning new technologies and implementing them in exciting projects.
-          <br></br>
-          I have also played violin for over 20 years and am an active musician.
-          <br></br>
-          <br></br>
-          I was a Software Engineer @ <a target="_blank" rel="noopener noreferrer" href="https://www.microsoft.com/en-us/Microsoft">Microsoft</a> from Jan 25, 2019 - Dec 11, 2023.
-        </p>
+        <p>Hello there! My name is Tabitha Hsia. I am a Full-Stack Software Engineer currently on a career break.</p>
+        <p>I enjoy challenging myself through learning new technologies and implementing them in exciting projects.</p>  
+        <p>I have also played violin for over 20 years and am an active musician.</p>
+        <p>I was a Software Engineer @ <a target="_blank" rel="noopener noreferrer" href="https://www.microsoft.com/en-us/Microsoft">Microsoft</a> from Jan 25, 2019 - Dec 11, 2023.</p>  
         <a target="_blank" rel="noopener noreferrer" href ="https://docs.google.com/document/d/1h3Kdyj4e3zcYmBM7S04EFD9_Q00oaBA3XLgHxWgFKU0/preview">Tech Resume</a> | <a target="_blank" rel="noopener noreferrer" href="http://bit.ly/tabithahsiamusicresume">Music Resume</a>
-        <h3 id="skills"> Skills </h3>
+        <h3> Skills </h3>
         <p><strong>Strong</strong>: TypeScript, JavaScript ES6, C#, ReactJS, Redux, Webpack, Node.js, Express, SQL, PostgreSQL, SQLite, MySQL, MongoDB, Mongoose, Git, HTML5, CSS3, jQuery, Bluebird</p>
         <p><strong>Knowledgeable</strong>: AngularJS, Python, Backbone.js, D3.js, C++, WebSockets, Socket.IO, Jekyll, Heroku, AWS, KSP, Ruby, KnockoutJS</p>
 
-        <h3 id="about"> Education </h3>
+        <h3>Education</h3>
         <p><strong>University of the Pacific, </strong>  B.A. in Music Violin & Asian Languages and Studies: Chinese, Minor in Pre-Law
           <br></br>
           <span id="italicize">Relevant Courses: Intro to Computer Science (C++), Data Structures</span>
@@ -35,14 +25,14 @@ class About extends React.Component {
           <br></br>
           <span id="italicize">Relevant Courses: Calculus I, Calculus II, Linear Algebra</span>
         </p>
-        <h3 id="about">Fun Facts</h3>
-        <li>I am a curry and fried chicken addict</li>
-        <li>Favorite aquatic animal is the whale shark</li>
-        <li>My dream job is to be the Mickey Mouse in Fantasmic</li>
-
+        <h3>Fun Facts</h3>
+        <ul>
+          <li>I am a curry and fried chicken addict</li>
+          <li>Favorite aquatic animal is the whale shark</li>
+          <li>My dream job is to be the Mickey Mouse in Fantasmic</li>
+        </ul>
       </div>
     )
   }
-}
 
 export default About;
