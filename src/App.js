bf647d2fe70d8bdc7a03c72907651c92c295c581
Tabitha <tabithahsia@gmail.com>
2024-10-21 20:16:10 -0700

    Refactored App.js to remove redundancy

--- src/App.js
+++ src/App.js
@@ -12,108 +12,61 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showAbout: false,
-      showPortfolio: false,
-      showContact: false,
-      showMusic: false,
-      open: false
+      activeModal: null
     }
-    this.handleAbout = this.handleAbout.bind(this);
-    this.handlePortfolio = this.handlePortfolio.bind(this);
-    this.handleContact = this.handleContact.bind(this);
-    this.handleMusic = this.handleMusic.bind(this);
   }
 
-  handleAbout () {
-    this.setState({
-      showAbout: true,
-      showPortfolio: false,
-      showContact: false,
-      showMusic: false,
-      open: true
-    })
-  }
-  handlePortfolio () {
-    this.setState({
-      showPortfolio: true,
-      showAbout: false,
-      showContact: false,
-      showMusic: false,
-      open: true
-    })
-  }
-
-  handleContact () {
-    this.setState({
-      showContact: true,
-      showAbout: false,
-      showPortfolio: false,
-      showMusic: false,
-      open: true
-    })
-  }
-
-  handleMusic () {
-    this.setState({
-      showMusic: true,
-      showAbout: false,
-      showContact: false,
-      showPortfolio: false,
-      open: true
-    })
-  }
-
-  onOpenModal = () => {
-    this.setState({ open: true });
+  handleModal = (modalName) => {
+    this.setState({ activeModal: modalName });
   };
 
   onCloseModal = () => {
-    this.setState({ open: false });
+    this.setState({ activeModal: null });
+  };
+
+  renderModalContent = () => {
+    const { activeModal } = this.state;
+    switch (activeModal) {
+      case 'About':
+        return <About />;
+      case 'Portfolio':
+        return <Portfolio />;
+      case 'Contact':
+        return <Contact />;
+      case 'Music':
+        return <Music />;
+      default:
+        return null;
+    }
   };
 
   render() {
-    const { open } = this.state;
+    const { activeModal } = this.state;
+    
     return (
       <div>
         <h1>Tabitha Hsia, Full-Stack Software Engineer & Violinist </h1>
-        <hr></hr>
-        <br></br>
+        <hr/>
         <div className="container">
-          <button className="item" onClick={ () => this.handleAbout() }>About</button>
-          <button className="item" onClick={ () => this.handlePortfolio() }>Portfolio</button>
-          <button className="item" onClick={ () => this.handleContact() }>Contact</button>
-          <button className="item" onClick={ () => this.handleMusic() }>Music</button>
-
-          { this.state.showAbout ?
-            <Modal open={open} onClose={this.onCloseModal} > <About /> </Modal>
-            :
-            null
-          }
-          { this.state.showPortfolio ?
-             <Modal open={open} onClose={this.onCloseModal} > <Portfolio /> </Modal>
-             :
-             null
-          }
-          { this.state.showContact ?
-            <Modal open={open} onClose={this.onCloseModal} little> <Contact /> </Modal>
-            :
-            null
-          }
-          {
-            this.state.showMusic ?
-            <Modal open={open} onClose={this.onCloseModal}> <Music /> </Modal>
-            :
-            null
-          }
-
+          {['About', 'Portfolio', 'Contact', 'Music'].map((modal) => (
+              <button
+                key={modal}
+                className="item"
+                onClick={() => this.handleModal(modal)}
+              >
+                {modal}
+              </button>
+            ))}
         </div>
+
+        <Modal open={!!activeModal} onClose={this.onCloseModal} center>
+          {this.renderModalContent()}
+        </Modal>
 
         <div id="image">
           <a href="https://github.com/tabithahsia" target="_blank" rel="noopener noreferrer"><img src={Github} alt="My Github" className="icon"/></a>
           <a href="https://linkedin.com/in/tabithahsia" target="_blank" rel="noopener noreferrer"><img src={Linkedin} alt="My Linkedin" className="icon"/></a>
         </div>
-
-
       </div>
     );
   }
