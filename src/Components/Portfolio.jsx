76f24f9658cdb1008ffca7c9141d196d17fabcc6
Tabitha <tabithahsia@gmail.com>
2024-10-21 20:19:49 -0700

    Refactored Portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -1,118 +1,103 @@
 import React from 'react';
 
-//Photos from Buzz
-import BuzzHome from '../assets/images/Buzz Home.png';
-import BuzzHomeLarge from '../assets/images/Buzz Home Large.png';
-import BuzzCategories from '../assets/images/Buzz Categories.png';
-import BuzzCategoriesLarge from '../assets/images/Buzz Categories Large.png';
-import BuzzHHList from '../assets/images/Buzz HH List.png';
-import BuzzHHListLarge from '../assets/images/Buzz HH List Large.png';
+import {
+  BuzzHome, BuzzHomeLarge, BuzzCategories, BuzzCategoriesLarge, BuzzHHList, BuzzHHListLarge,
+  JCFHome, JCFHomeLarge,
+  iResistProtests, iResistProtestsLarge, iResistProtestInfo, iResistProtestInfoLarge,
+  iResistMap, iResistMapLarge, iResistMessages, iResistMessagesLarge, iResistTweets, iResistTweetsLarge,
+  CatFacts, CatFactsLarge,
+  Yahoogle, YahoogleLarge,
+} from '../assets/images'; 
 
-//Photos from JCF Page
-import JCFHome from '../assets/images/JCF Home.png';
-import JCFHomeLarge from '../assets/images/JCF Home Large.png';
+const Project = ({ title, description, technologies, demoLink, repoLink, images }) => (
+  <div className="project">
+    <h3>{title}</h3>
+    <p>{description}</p>
+    <p className="italicize">{title} is built with {technologies}</p>
+    <span>
+      Check it out at <a href={demoLink} target="_blank" rel="noopener noreferrer">{demoLink}</a> and see the code on{' '}
+      <a href={repoLink} target="_blank" rel="noopener noreferrer">Github</a>
+    </span>
+    <div className="project-images">
+      {images.map(({ small, large, alt }) => (
+        <a key={alt} href={large} target="_blank" rel="noopener noreferrer">
+          <img src={small} alt={alt} />
+        </a>
+      ))}
+    </div>
+  </div>
+);
 
-//Photos from iResist
-import iResistProtests from '../assets/images/iResist Protests.png';
-import iResistProtestsLarge from '../assets/images/iResist Protests Large.png';
-import iResistProtestInfo from '../assets/images/iResist Protest Info.png';
-import iResistProtestInfoLarge from '../assets/images/iResist Protest Info Large.png';
-import iResistMap from '../assets/images/iResist Map.png';
-import iResistMapLarge from '../assets/images/iResist Map Large.png';
-import iResistMessages from '../assets/images/iResist Messages.png';
-import iResistMessagesLarge from '../assets/images/iResist Messages Large.png';
-import iResistTweets from '../assets/images/iResist Tweets.png';
-import iResistTweetsLarge from '../assets/images/iResist Tweets Large.png';
+const Portfolio = () => {
+  const projects = [
+    {
+      title: 'iResist',
+      description: `iResist allows users to anonymously organize, execute, or join protests.
+        On the day of the protest, participants can view a live message feed, drop pins on a map, and follow a Twitter feed.`,
+      technologies: 'React, Redux, Socket.io, Leaflet, Twit, NodeJS, ExpressJS, PostgreSQL, Heroku',
+      demoLink: 'https://i-resist.herokuapp.com',
+      repoLink: 'https://github.com/iResist/iResist',
+      images: [
+        { small: iResistProtests, large: iResistProtestsLarge, alt: 'iResist Protests' },
+        { small: iResistProtestInfo, large: iResistProtestInfoLarge, alt: 'iResist Protest Info' },
+        { small: iResistMap, large: iResistMapLarge, alt: 'iResist Map' },
+        { small: iResistMessages, large: iResistMessagesLarge, alt: 'iResist Messages' },
+        { small: iResistTweets, large: iResistTweetsLarge, alt: 'iResist Tweets' },
+      ],
+    },
+    {
+      title: 'Buzz: SF Happy Hour Finder',
+      description: `Buzz allows users to find happy hours in SF neighborhoods. 
+        Users can select a neighborhood or choose a category (Divey, Bougie, Hip, etc.) to see recommendations.`,
+      technologies: 'React, NodeJS, ExpressJS, PostgreSQL, Heroku',
+      demoLink: 'https://buzzhappyhour.herokuapp.com/',
+      repoLink: 'https://github.com/BuzzHappyHour/Buzz',
+      images: [
+        { small: BuzzHome, large: BuzzHomeLarge, alt: 'Buzz Homepage' },
+        { small: BuzzCategories, large: BuzzCategoriesLarge, alt: 'Buzz Categories' },
+        { small: BuzzHHList, large: BuzzHHListLarge, alt: 'Buzz HH List' },
+      ],
+    },
+    {
+      title: 'Journey Christian Fellowship Homepage',
+      description: `This is the website for Journey Christian Fellowship, a church in Walnut Creek.`,
+      technologies: 'Jekyll and Heroku',
+      demoLink: 'http://journeychristianfellowship.herokuapp.com/',
+      repoLink: 'https://github.com/tabithahsia/churchwebsite',
+      images: [
+        { small: JCFHome, large: JCFHomeLarge, alt: 'JCF Homepage' },
+      ],
+    },
+    {
+      title: 'Cat Facts',
+      description: `Cat Facts generates cat facts and allows you to share these amazing facts with your fellow human beings.`,
+      technologies: 'React, NodeJS, ExpressJS, PostgreSQL, Heroku',
+      demoLink: 'http://catfacts.life',
+      repoLink: 'https://github.com/tabithahsia/Cat-Facts',
+      images: [
+        { small: CatFacts, large: CatFactsLarge, alt: 'Cat Facts' },
+      ],
+    },
+    {
+      title: 'YahoogleMail',
+      description: `YahoogleMail is an email service.`,
+      technologies: 'React, NodeJS, ExpressJS, Heroku, SendGrid, Mailgun',
+      demoLink: 'http://yahooglemail.herokuapp.com',
+      repoLink: 'https://github.com/tabithahsia/email-service',
+      images: [
+        { small: Yahoogle, large: YahoogleLarge, alt: 'YahoogleMail' },
+      ],
+    },
+  ];
 
-//Photos from Cat Facts
-import CatFacts from '../assets/images/Cat Facts.png';
-import CatFactsLarge from '../assets/images/Cat Facts Large.png';
-
-//Photo from Yahoogle Mail
-import Yahoogle from '../assets/images/Yahoogle.png';
-import YahoogleLarge from '../assets/images/Yahoogle-Large.png';
-
-class Portfolio extends React.Component {
-  render() {
-    return (
-      <div>
-        <h2>Portfolio</h2>
-
-        <h3> iResist </h3>
-        <p>
-          iResist allows users to anonymously organize, execute, or join protests.
-          On the day of the protest, participants are able to view a live message feed, drop pins on a map alerting others of happenings, and view a Twitter feed.
-        </p>
-        <p id="italicize">
-          iResist is built with React, Redux, Socket.io, Leaflet, Twit, NodeJS, ExpressJS, PostgreSQL, Heroku
-        </p>
-        <span> Check it out at <a href="https://i-resist.herokuapp.com" target="_blank" rel="noopener noreferrer">i-resist.herokuapp.com</a> and see the code on <a href="https://github.com/iResist/iResist" target="_blank" rel="noopener noreferrer" >Github</a></span>
-        <br/>
-        <br/>
-        <a target="_blank" href={iResistProtestsLarge}><img src={iResistProtests} alt="iResist Protests" /></a>
-        <a target="_blank" href={iResistProtestInfoLarge} rel="noopener"><img src={iResistProtestInfo} alt="iResist Protests Info" /></a>
-        <a target="_blank" href={iResistMapLarge} rel="noopener"><img src={iResistMap} alt="iResist Map" /></a>
-        <a target="_blank" href={iResistMessagesLarge} rel="noopener"><img src={iResistMessages} alt="iResist Messages" /></a>
-        <a target="_blank" href={iResistTweetsLarge} rel="noopener"><img src={iResistTweets} alt="iResist Tweets" /></a>
-
-      <h3>Buzz: SF Happy Hour Finder </h3>
-      <p>
-        Buzz allows users to find happy hours in SF neighborhoods. Users can select a neighborhood or choose a category (Divey, Bougie, Hip, etc.) and see recommendations. This app was created because Yelp did not have information on what time happy hours occurred.
-        <br/>
-        <br/>
-        Turn your "sad hours" into "happy hours"
-      </p>
-      <p id="italicize">
-        Buzz is built with React, NodeJS, ExpressJS, PostgreSQL, Heroku
-      </p>
-      <span> Check it out at <a href="https://buzzhappyhour.herokuapp.com/" target="_blank" rel="noopener noreferrer">buzzhappyhour.herokuapp.com</a> and see the code on <a href="https://github.com/BuzzHappyHour/Buzz" target="_blank" rel="noopener noreferrer">Github</a></span>
-      <br/>
-      <br/>
-
-      <a target="_blank" href={BuzzHomeLarge} rel="noopener"><img src={BuzzHome} alt="Buzz Homepage" /></a>
-      <a target="_blank" href={BuzzCategoriesLarge} rel="noopener"><img src={BuzzCategories} alt="Buzz Categories" /></a>
-      <a target="_blank" href={BuzzHHListLarge} rel="noopener"><img src={BuzzHHList} alt="Buzz HH List" /></a>
-
-      <h3>Journey Christian Fellowship Homepage</h3>
-      <p>
-        This is the website for Journey Christian Fellowship, a church in Walnut Creek.
-      </p>
-      <p id="italicize">
-        JCF's homepage is built with Jekyll and Heroku
-      </p>
-      <span> Check it out at <a href="http://journeychristianfellowship.herokuapp.com/" target="_blank" rel="noopener noreferrer">journeychristianfellowship.herokuapp.com</a> and see the code on <a href="https://github.com/tabithahsia/churchwebsite" target="_blank" rel="noopener noreferrer">Github</a></span>
-        <br/>
-        <br/>
-      <a target="_blank" href={JCFHomeLarge} rel="noopener"><img src={JCFHome} alt="JCF Homepage" /></a>
-
-      <h3>Cat Facts</h3>
-      <p>
-        Cat Facts generates cat facts and allows you to share these amazing facts with your fellow human beings.
-      </p>
-      <p id="italicize">
-        Cat Facts is built with React, NodeJS, ExpressJS, PostgreSQL, and Heroku
-      </p>
-      <span> Check it out at <a href="http://catfacts.life" target="_blank" rel="noopener noreferrer">catfacts.life</a> and see the code on <a href="https://github.com/tabithahsia/Cat-Facts" target="_blank" rel="noopener noreferrer">Github</a></span>
-        <br/>
-        <br/>
-      <a target="_blank" href={CatFactsLarge} rel="noopener"><img src={CatFacts} alt="Cat Facts" /></a>
-
-      <h3>YahoogleMail</h3>
-      <p>
-        YahoogleMail is an email service.
-      </p>
-      <p id="italicize">
-        YahoogleMail is built with React, NodeJS, ExpressJS, Heroku, SendGrid, and Mailgun
-      </p>
-      <span> Check it out at <a href="http://yahooglemail.herokuapp.com" target="_blank" rel="noopener noreferrer">yahooglemail.herokuapp.com</a> and see the code on <a href="https://github.com/tabithahsia/email-service" target="_blank" rel="noopener noreferrer">Github</a></span>
-        <br/>
-        <br/>
-      <a target="_blank" href={YahoogleLarge} rel="noopener"><img src={Yahoogle} alt="Cat Facts" /></a>
-
-      </div>
-
-    )
-  }
+  return (
+    <div className="portfolio">
+      <h2>Portfolio</h2>
+      {projects.map((project) => (
+        <Project key={project.title} {...project} />
+      ))}
+    </div>
+  );
 }
 
 export default Portfolio;
