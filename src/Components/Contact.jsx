d2db5e238eaabf609906c672b120cb7a76d5f488
Tabitha <tabithahsia@gmail.com>
2025-07-24 09:39:48 -0700

    Refactored contact form code and added labels

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,55 +1,52 @@
-import { Component } from "react";
-
-class Contact extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Contact Me</h2>
-        <p>
-          {" "}
-          Contact me at{" "}
-          <a href="mailto:tabithahsia@gmail.com">tabithahsia@gmail.com</a>{" "}
-        </p>
-        <p> or fill out the form below </p>
-        <form
-          action="https://formspree.io/tabithahsia@gmail.com"
-          method="POST"
-          class="submissionfield"
-        >
-          <input type="text" name="Name" placeholder="Your Name* " required />
-
-          <br></br>
-          <br></br>
-
-          <input
-            type="email"
-            name="Email"
-            placeholder="Your Email* "
-            required
-          />
-
-          <br></br>
-          <br></br>
-
-          <input
-            type="tel"
-            name="Phone Number"
-            placeholder="Your Phone (optional)"
-          />
-
-          <br></br>
-          <br></br>
-
-          <textarea name="Message" placeholder="Your Message*" required />
-
-          <br></br>
-          <br></br>
-
-          <input type="submit" value="Submit" />
-        </form>
-      </div>
-    );
-  }
-}
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Me</h2>
+      <p>
+        Contact me at{" "}
+        <a href="mailto:tabithahsia@gmail.com">tabithahsia@gmail.com</a> or fill
+        out the form below.
+      </p>
+      <form
+        action="https://formspree.io/tabithahsia@gmail.com"
+        method="POST"
+        class="submissionfield"
+      >
+        <label htmlFor="Name">Name (required)</label>
+        <input
+          id="contact-name"
+          type="text"
+          name="Name"
+          placeholder="Bob Ross"
+          required
+        />
+        <label htmlFor="contact-email">Email (required)</label>
+        <input
+          id="contact-email"
+          type="email"
+          name="Email"
+          placeholder="bobross@gmail.com "
+          required
+        />
+        <label htmlFor="contact-phone">Phone Number (optional)</label>
+        <input
+          id="contact-phone"
+          type="tel"
+          name="Phone Number"
+          placeholder="(123) 456-7890"
+        />
+        <label htmlFor="contact-message">Message (required)</label>
+        <br></br>
+        <textarea
+          id="contact-message"
+          name="Message"
+          placeholder="Hello! Have a wonderful day!"
+          required
+        />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
